619758afe625064b920b0db8198082ce1447139a
Nathalya Ohana Pires Ferreira <nathyohana@gmail.com>
2023-03-24 22:22:26 -0400

    feat: rota post implementada e funcional

---
+++ src/controllers/cakes.controller.js
@@ -0,0 +1,25 @@
+import { db } from "../database/database.js";
+
+export async function createCake(req, res) {
+  const { name, price, description, image } = req.body;
+
+  try {
+    const cakeExist = await db.query(`SELECT * FROM cakes WHERE name = $1`, [
+      name,
+    ]);
+
+    // console.log(cakeExist);
+
+    if (cakeExist.rowCount !== 0) return res.sendStatus(409);
+
+    await db.query(
+      `INSERT INTO cakes (name, price, description, image) VALUES ($1, $2, $3, $4)`,
+      [name, price, description, image]
+    );
+
+    res.sendStatus(201);
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+}
--- src/schemas/cakesSchema.js
+++ src/schemas/cakesSchema.js
@@ -4,10 +4,7 @@ const cakesSchema = joi.object({
   name: joi.string().required().min(2),
   price: joi.number().positive().required(),
   description: joi.string().optional().allow(null),
-  image: joi
-    .string()
-    .uri({ scheme: ["http", "https"] })
-    .required(),
+  image: joi.string().uri().required()
 });
 
 export default cakesSchema;
