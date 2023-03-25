a793c463ee42b793c6e9a86e623a2430547e1c96
Nathalya Ohana Pires Ferreira <nathyohana@gmail.com>
2023-03-24 22:44:44 -0400

    revert: corrigindo commit

--- src/middlewares/validateSchema.js
+++ src/middlewares/validateSchema.js
@@ -3,8 +3,17 @@ export function validateSchema(schema) {
     const { error } = schema.validate(req.body, { abortEarly: false });
 
     if (error) {
+      const imageError = error.details.find((error) =>
+        error.path.includes("image")
+      );
+      if (imageError) {
+        return res.status(422).send(imageError.message);
+      }
+    }
+
+    if (error) {
       return res
-        .status(422)
+        .status(400)
         .send(error.details.map((detail) => detail.message));
     }
 
--- src/routes/cakes.routes.js
+++ src/routes/cakes.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
+import { createCake } from "../controllers/cakes.controller.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import cakesSchema from "../schemas/cakesSchema.js";
 
 const cakesRouter = Router();
 
-cakesRouter.post("/cakes", validateSchema(cakesSchema), () => {});
+cakesRouter.post("/cakes", validateSchema(cakesSchema), createCake );
 
 export default cakesRouter;
+
+
