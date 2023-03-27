f9161ed1ec25e06a89bb6cd72f7567b3b5c98cff
Nathalya Ohana Pires Ferreira <nathyohana@gmail.com>
2023-03-27 01:55:20 -0400

    feat: get ordersByClient implementada

--- src/controllers/clients.controller.js
+++ src/controllers/clients.controller.js
@@ -15,3 +15,29 @@ export async function createClients(req, res) {
     res.sendStatus(500);
   }
 }
+
+export async function getOrdersByClient(req, res) {
+  const { id } = req.params;
+
+  try {
+    const result = await db.query(
+      `SELECT json_build_object
+    ('orderId', orders.id, 'quantity', orders.quantity, 'createdAt', TO_CHAR(orders."createdAt"::TIMESTAMP AT TIME ZONE 'UTC', 'YYYY-MM-DD HH24:MI'),
+    'totalPrice', orders."totalPrice", 'cakeName', cakes.name) AS order
+    FROM orders
+    JOIN clients ON orders."clientId" = clients.id
+    JOIN cakes ON orders."cakeId" = cakes.id
+    WHERE clients.id = $1`,
+      [id]
+    );
+
+    if (result.rowCount === 0) {
+      res.status(404).send("Id do cliente não existe");
+    } else {
+      res.status(200).send(result.rows.map((row) => row.order));
+    }
+  } catch (error) {
+    console.error(error);
+    res.sendStatus(500);
+  }
+}
--- src/routes/clients.routes.js
+++ src/routes/clients.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import { createClients } from "../controllers/clients.controller.js";
+import { createClients, getOrdersByClient } from "../controllers/clients.controller.js";
 import { validateSchema } from "../middlewares/validateSchema.js";
 import clientsSchema from "../schemas/clientsSchema.js";
 
 const clientsRouter = Router();
 
 clientsRouter.post("/clients", validateSchema(clientsSchema), createClients);
-clientsRouter.get("/clients/:id/orders", () => {});
+clientsRouter.get("/clients/:id/orders", getOrdersByClient);
 
 export default clientsRouter;
